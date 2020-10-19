8d51e1e4a911a25e06ee76d8aa59daa59743b791
kofer1001 <kofergra@gmail.com>
2020-10-19 11:10:53 +0200

    Restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
-
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit,:update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
-
+  before_action :set_user, only: [:show,:edit,:update]
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -15,11 +16,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = 'Your account information was succesfully updated'
       redirect_to @user
@@ -31,6 +30,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the  Kofer blog #{@user.username}, you are my new user :D"
       redirect_to articles_path
     else
@@ -38,9 +38,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "account and all associated articles soccessfully deleted"
+    redirect_to root_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own account"
+    end
   end
 end
