153ffb96a48b019074c2b7fc17a6dc7351dc69d6
kofer1001 <kofergra@gmail.com>
2020-10-21 14:07:59 +0200

    MA

--- db/migrate/20201021112643_add_users_c.rb
+++
@@ -1,8 +0,0 @@
-class AddUsersC < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :username, :string
-    add_column :users, :email, :string
-    add_column :users, :created_at, :datetime, precision: 6, null: false
-    add_column :users, :updated_at, :datetime, precision: 6, null: false
-  end
-end
